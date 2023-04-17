264077cec605801c4d302d100e3e5ead9ff433d8
Sven Jansen <sven.jansen@devicetrust.com>
2023-04-17 15:06:47 +0200

    .

--- main.tf
+++ main.tf
@@ -2,24 +2,25 @@ locals {
   httpprofilename = "http_prof_${var.adc-base.environmentname}"
   tcpprofilename  = "tcp_prof_${var.adc-base.environmentname}"
   sslprofilename  = "ssl_prof_${var.adc-base.environmentname}_fe_TLS1213"
+
 }
 
 #####
 # Add LB Server
 #####
 resource "citrixadc_server" "lb_server" {
-  count     = length(var.adc-lb-srv.name)
-  name      = "lb_srv_${element(var.adc-lb-srv["name"],count.index)}"
-  ipaddress = element(var.adc-lb-srv["ip"],count.index)
+  count     = length(var.adc-lb.lb_srv_name)
+  name      = "lb_srv_${element(var.adc-lb["lb_srv_name"],count.index)}"
+  ipaddress = element(var.adc-lb["lb_srv_ip"],count.index)
 }
 
 #####
 # Add LB Service Groups
 #####
 resource "citrixadc_servicegroup" "lb_servicegroup" {
-  count             = length(var.adc-lb.name)
-  servicegroupname  = "lb_sg_${element(var.adc-lb["name"],count.index)}_${element(var.adc-lb["type"],count.index)}_${element(var.adc-lb["port"],count.index)}"
-  servicetype       = element(var.adc-lb["type"],count.index)
+  count             = length(var.adc-lb.lb_name)
+  servicegroupname  = "lb_sg_${element(var.adc-lb["lb_name"],count.index)}_${element(var.adc-lb["lb_type"],count.index)}_${element(var.adc-lb["lb_port"],count.index)}"
+  servicetype       = element(var.adc-lb["lb_type"],count.index)
 
   depends_on = [
     citrixadc_server.lb_server
@@ -30,10 +31,10 @@ resource "citrixadc_servicegroup" "lb_servicegroup" {
 # Bind LB Server to Service Groups
 #####
 resource "citrixadc_servicegroup_servicegroupmember_binding" "lb_sg_server_binding" {
-  count             = length(var.adc-lb.name)
-  servicegroupname  = "lb_sg_${element(var.adc-lb["name"],count.index)}_${element(var.adc-lb["type"],count.index)}_${element(var.adc-lb["port"],count.index)}"
-  servername        = "lb_srv_${element(var.adc-lb["backend-server"],count.index)}"
-  port              = element(var.adc-lb["port"],count.index)
+  count             = length(var.adc-lb.lb_name)
+  servicegroupname  = "lb_sg_${element(var.adc-lb["lb_name"],count.index)}_${element(var.adc-lb["lb_type"],count.index)}_${element(var.adc-lb["lb_port"],count.index)}"
+  servername        = "lb_srv_${element(var.adc-lb["lb_srv_name"],count.index)}"
+  port              = element(var.adc-lb["lb_port"],count.index)
 
   depends_on = [
     citrixadc_servicegroup.lb_servicegroup
@@ -44,18 +45,18 @@ resource "citrixadc_servicegroup_servicegroupmember_binding" "lb_sg_server_bindi
 # Add and configure LB vServer
 #####
 resource "citrixadc_lbvserver" "lb_vserver" {
-  count           = length(var.adc-lb.name)
-  name            = "lb_vs_${element(var.adc-lb["name"],count.index)}_${element(var.adc-lb["type"],count.index)}_${element(var.adc-lb["port"],count.index)}"
+  count           = length(var.adc-lb.lb_name)
+  name            = "lb_vs_${element(var.adc-lb["lb_name"],count.index)}_${element(var.adc-lb["lb_type"],count.index)}_${element(var.adc-lb["lb_port"],count.index)}"
 
-  servicetype     = element(var.adc-lb["type"],count.index)
+  servicetype     = element(var.adc-lb["lb_type"],count.index)
   ipv46           = var.adc-lb-generic.lb-ip
   port            = var.adc-lb-generic.lb-port
   lbmethod        = var.adc-lb-generic.lbmethod
   persistencetype = var.adc-lb-generic.persistencetype
   timeout         = var.adc-lb-generic.timeout
-  sslprofile      = element(var.adc-lb["type"],count.index) == "SSL" ? local.sslprofilename : null
-  httpprofilename = element(var.adc-lb["type"],count.index) == "DNS" || element(var.adc-lb["type"],count.index) == "TCP" ? null : local.httpprofilename
-  tcpprofilename  = element(var.adc-lb["type"],count.index) == "DNS" ? null : local.tcpprofilename
+  sslprofile      = element(var.adc-lb["lb_type"],count.index) == "SSL" ? local.sslprofilename : null
+  httpprofilename = element(var.adc-lb["lb_type"],count.index) == "DNS" || element(var.adc-lb["lb_type"],count.index) == "TCP" ? null : local.httpprofilename
+  tcpprofilename  = element(var.adc-lb["lb_type"],count.index) == "DNS" ? null : local.tcpprofilename
 
   depends_on = [
     citrixadc_servicegroup_servicegroupmember_binding.lb_sg_server_binding
@@ -66,9 +67,9 @@ resource "citrixadc_lbvserver" "lb_vserver" {
 # Bind LB Service Groups to LB vServers
 #####
 resource "citrixadc_lbvserver_servicegroup_binding" "lb_vserver_sg_binding" {
-  count             = length(var.adc-lb.name)
-  name              = "lb_vs_${element(var.adc-lb["name"],count.index)}_${element(var.adc-lb["type"],count.index)}_${element(var.adc-lb["port"],count.index)}"
-  servicegroupname  = "lb_sg_${element(var.adc-lb["name"],count.index)}_${element(var.adc-lb["type"],count.index)}_${element(var.adc-lb["port"],count.index)}"
+  count             = length(var.adc-lb.lb_name)
+  name              = "lb_vs_${element(var.adc-lb["lb_name"],count.index)}_${element(var.adc-lb["lb_type"],count.index)}_${element(var.adc-lb["lb_port"],count.index)}"
+  servicegroupname  = "lb_sg_${element(var.adc-lb["lb_name"],count.index)}_${element(var.adc-lb["lb_type"],count.index)}_${element(var.adc-lb["lb_port"],count.index)}"
 
   depends_on = [
     citrixadc_lbvserver.lb_vserver
